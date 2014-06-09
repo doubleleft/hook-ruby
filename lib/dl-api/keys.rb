effee1fbcd85a73e455dca196482a3b166714019
Endel Dreyer <endel.dreyer@gmail.com>
2014-06-09 12:05:02 -0300

    cover key/values with tests

--- lib/dl-api/keys.rb
+++ lib/dl-api/keys.rb
@@ -1,4 +1,18 @@
 module DL
   class Keys
+
+    def initialize(options={})
+      @client = options.delete(:client) || DL::Client.instance
+    end
+
+    def get(key)
+      response = @client.get("key/#{key}");
+      (!response.empty?) ? response['value'] : nil
+    end
+
+    def set(key, value)
+      @client.post("key/#{key}", { :value => value });
+    end
+
   end
 end
---
+++ spec/keys_spec.rb
@@ -0,0 +1,20 @@
+describe DL::Keys do
+
+  subject do
+    DL::Client.instance
+  end
+
+  it "should get and set keys" do
+    expect(subject.keys.get(:inexistent)).to be_nil
+
+    subject.keys.set :numeric, 12345
+    expect(subject.keys.get(:numeric)).to be == '12345'
+
+    subject.keys.set :float, 19.99
+    expect(subject.keys.get(:float)).to be == '19.99'
+
+    subject.keys.set :string, 'hello there!'
+    expect(subject.keys.get(:string)).to be == 'hello there!'
+  end
+end
+
